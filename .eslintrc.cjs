584ca5f5038baec1f61a9f9c57ad41d9e772d759
Ryan Killeen <ryan@ryankilleen.com>
2023-01-26 21:14:28 -0500

    style: add initial eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
