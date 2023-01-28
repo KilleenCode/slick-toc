e9c1e68db58f87d4bd94160dd17e1148131f468a
Ryan Killeen <ryan@ryankilleen.com>
2023-01-28 13:30:30 -0500

    fix: exclude jsx runtime from output

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react", "react-dom", "react-jsx-runtime.production"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
